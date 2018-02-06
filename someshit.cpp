29519544b660ef8be5446c8b66c050b7c0fca02d
Lukas <lukislt7@gmail.com>
2018-02-06 20:11:33 +0200

    updated code

--- someshit.cpp
+++ someshit.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 string generateHorizontalLine(int length){
 string line;
-for (int i = 0; i < length; i++)
+for (int i = 0; i < length+4; i++)
     line += "*";
 return line;
 }
@@ -12,7 +13,7 @@ return line;
 string generateVerticalLine(int length){
 string line;
 line += "*";
-for (int i = 0; i < length-2; i++)
+for (int i = 0; i < length+2; i++)
     line += " ";
 line += "*";
 return line;
@@ -26,17 +27,35 @@ line += " *";
 return line;
 }
 
-void generateInterface(int length){
-cout << generateHorizontalLine(5) << endl;
-cout << generateVerticalLine(5) << endl;
-cout << generateNameLine("cock") << endl;
-cout << generateVerticalLine(5) << endl;
-cout << generateHorizontalLine(5) << endl;
+string generateNameString (string name, int isMale){
+string line;
+if (isMale == 1)
+    line += "Sveikas, ";
+else if (isMale == 2)
+    line += "Sveika, ";
+else line += "Sveiki, ";
+return line += name;
+}
+
+void generateInterface(string name, int isMale){
+int length = generateNameString(name, isMale).length();
+cout << generateHorizontalLine(length) << endl;
+cout << generateVerticalLine(length) << endl;
+cout << generateNameLine(generateNameString(name, isMale)) << endl;
+cout << generateVerticalLine(length) << endl;
+cout << generateHorizontalLine(length) << endl;
+}
+
+bool isMale(string name){
+    if(name.at(name.length()-1) == 's')
+    return 1;
+    else if(name.at(name.length()-1) == 'a' || name.at(name.length()-1) == 'e')
+    return 2;
+    else return 0;
 }
 
 int main(){
-
-cout << generateHorizontalLine(5) << endl;
-cout << generateVerticalLine(5) << endl;
-cout << generateNameLine("cock") << endl;
+    string name;
+    cout << "Iveskite varda: "; cin >> name;
+    generateInterface(name, isMale(name));
 }
