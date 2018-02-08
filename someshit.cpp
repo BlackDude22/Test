1a749b16cb6f5820fc6bb547aad22f54382eb55f
Lukas <lukislt7@gmail.com>
2018-02-08 12:58:30 +0200

    nu tipo padariau

--- someshit.cpp
+++ someshit.cpp
@@ -1,56 +1,41 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
-string generateHorizontalLine(int length){
-string line;
-for (int i = 0; i < length+4; i++)
-    line += "*";
-return line;
-}
-
-string generateVerticalLine(int length){
-string line;
-line += "*";
-for (int i = 0; i < length+2; i++)
-    line += " ";
-line += "*";
-return line;
-}
-
-string generateNameLine(string name){
-string line;
-line += "* ";
-line += name;
-line += " *";
-return line;
-}
+using std::cout; using std::cin; using std::endl; using std::string;
 
 string generateNameString (string name, int isMale){
-string line;
 if (isMale == 1)
-    line += "Sveikas, ";
+    return "Sveikas, " + name;
 else if (isMale == 2)
-    line += "Sveika, ";
-else line += "Sveiki, ";
-return line += name;
+    return "Sveika, " + name;
+else return "Sveiki, " +  name;
 }
 
 void generateInterface(string name, int isMale){
-int length = generateNameString(name, isMale).length();
-cout << generateHorizontalLine(length) << endl;
-cout << generateVerticalLine(length) << endl;
-cout << generateNameLine(generateNameString(name, isMale)) << endl;
-cout << generateVerticalLine(length) << endl;
-cout << generateHorizontalLine(length) << endl;
+    for (int i = 0; i < 5; i++){
+        if (i == 0 || i == 4){
+            for (int j = 0; j < generateNameString(name, isMale).length()+4; j++)
+                cout << "*";
+            cout << endl;
+        }
+        else if (i == 1 || i == 3){
+            cout << "*";
+            for (int j = 0; j < generateNameString(name, isMale).length()+2; j++)
+                cout << " ";
+            cout << "*";
+            cout << endl;
+        }
+        else cout << "* " + generateNameString(name, isMale) + " *" << endl;
+    }
 }
 
-bool isMale(string name){
-    if(name.at(name.length()-1) == 's')
-    return 1;
-    else if(name.at(name.length()-1) == 'a' || name.at(name.length()-1) == 'e')
-    return 2;
+int isMale(string name){
+    if(name.at(name.length()-1) == 's'){
+        return 1;
+    }
+    else if(name.at(name.length()-1) == 'a' || name.at(name.length()-1) == 'e'){
+        return 2;
+    }
     else return 0;
 }
 
